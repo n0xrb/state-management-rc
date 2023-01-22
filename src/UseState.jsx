b7728b4c2df1f6cbfa56e8249d771d79ba990b61
Alex V <alex.v@nxrub.pw>
2023-01-22 17:00:52 -0300

    Efectos con useEffect

--- src/UseState.jsx
+++ src/UseState.jsx
@@ -1,15 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 export const UseState = ({ name }) => {
     const [error, setError] = useState(true);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        console.log('comienza efecto');
+        if (!!loading) {
+            setTimeout(() => setLoading(!loading), 3000);
+        }
+        console.log('termina efecto');
+    }, [loading]);
+
     return (
         <div>
             <h2>Eliminar {name}</h2>
             <p>Por favor, escribe el código de seguridad.</p>
 
             {error && <p>El código es incorrecto</p>}
+            {loading && <p>Cargando...</p>}
             <input placeholder="código de seguridad" type="text" />
-            <button onClick={() => setError(!error)}>Comprobar</button>
+            <button onClick={() => setLoading(!loading)}>Comprobar</button>
         </div>
     );
 };
